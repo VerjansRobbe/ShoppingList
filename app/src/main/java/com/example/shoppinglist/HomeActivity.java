28a17913cd2672ad47cf76d9f0455216cf2f2267
Robbe Verjans <56727648+VerjansRobbe@users.noreply.github.com>
2021-12-15 20:36:23 +0100

    added firebase read and write to database + small error in view that crashes the app

--- app/src/main/java/com/example/shoppinglist/HomeActivity.java
+++ app/src/main/java/com/example/shoppinglist/HomeActivity.java
@@ -1,20 +1,176 @@
 package com.example.shoppinglist;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import Model.Data;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+    private FloatingActionButton fab_button;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        toolbar=findViewById(R.id.home_toolbar);
-        setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Shopping list");
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser mUser = mAuth.getCurrentUser();
+
+        String uId = mUser.getUid();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Shopping list").child(uId);
+
+        mDatabase.keepSynced(true);
+
+
+        recyclerView=findViewById(R.id.recycler_home);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        fab_button=findViewById(R.id.fab);
+        fab_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customDialog();
+            }
+        });
+    }
+
+    private void customDialog()
+    {
+        AlertDialog.Builder mydialog=new AlertDialog.Builder(HomeActivity.this);
+        LayoutInflater inflater = LayoutInflater.from(HomeActivity.this);
+        View myview = inflater.inflate(R.layout.input_data, null);
+        AlertDialog dialog = mydialog.create();
+        dialog.setView(myview);
+
+        EditText name = myview.findViewById(R.id.edt_name);
+        EditText amount = myview.findViewById(R.id.edt_amount);
+        EditText note = myview.findViewById(R.id.edt_note);
+        Button saveButton = myview.findViewById(R.id.save_btn);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String mName=name.getText().toString().trim();
+                String mAmount = amount.getText().toString().trim();
+                String mNote = note.getText().toString().trim();
+
+                if(TextUtils.isEmpty(mName))
+                {
+                    name.setError("Required field");
+                    return;
+                }
+                if(TextUtils.isEmpty(mAmount))
+                {
+                    amount.setError("Required field");
+                    return;
+                }
+                if(TextUtils.isEmpty(mNote))
+                {
+                    note.setError("Required field");
+                    return;
+                }
+
+                String id = mDatabase.push().getKey();
+                String date = DateFormat.getDateInstance().format(new Date());
+                Data data = new Data(mName, mAmount, mNote, date, id);
+
+                mDatabase.child(id).setValue(data);
+
+                Toast.makeText(getApplicationContext(), "Data added", Toast.LENGTH_SHORT).show();
+
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Data, MyViewHolder>adapter=new FirebaseRecyclerAdapter<Data, MyViewHolder>(Data.class, R.layout.item_data, MyViewHolder.class, mDatabase) {
+            @Override
+            protected void populateViewHolder(MyViewHolder viewHolder, Data model, int i) {
+
+                viewHolder.setDate(model.getDate());
+                viewHolder.setName(model.getName());
+                viewHolder.setNote(model.getNote());
+                viewHolder.setAmount(model.getAmount());
+            }
+        };
+
+        recyclerView.setAdapter(adapter);
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        View myview;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            myview=itemView;
+        }
+
+        public void setName(String name){
+            TextView mName = myview.findViewById(R.id.name);
+            mName.setText(name);
+        }
+
+        public void setNote(String note){
+            TextView mNote = myview.findViewById(R.id.note);
+            mNote.setText(note);
+        }
+
+        public void setDate(String date){
+            TextView mDate = myview.findViewById(R.id.date);
+            mDate.setText(date);
+        }
+        
+        public void setAmount(String amount){
+            TextView mAmount = myview.findViewById(R.id.amount);
+            mAmount.setText(amount);
+        }
+
     }
 }
